4f05e9a39672ce28d1daf686c8acd0d260eb85fa
Jacob Shore <jacobshore@gmail.com>
2022-05-10 00:56:12 +0300

    html bool

--- refactor_example/main.py
+++ refactor_example/main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+
 from colorama import Fore
 
 import inventory
@@ -11,21 +12,51 @@ class OrderRow:
     quantity: int = 1
 
 
-def print_receipt(customer_name: str, item_list=List[OrderRow]) -> None:
+def print_receipt(
+    customer_name: str,
+    item_list=List[OrderRow],
+    html: bool = False,
+) -> None:
     balance = 0
-    print(Fore.CYAN + f"Receipt for \033[1m{customer_name}\033[0m")
-    print(Fore.YELLOW + "===========================================================")
-    print(Fore.WHITE + "\033[1mItems:\033[0m")
-    for list_item in item_list:
-        price = list_item.item.price * list_item.quantity
+    if not html:
+        print(Fore.CYAN + f"Receipt for \033[1m{customer_name}\033[0m")
         print(
-            f"{list_item.item.name}:\n\t Price: ${list_item.item.price/100:.2f} * Quantity: {list_item.quantity} = ${price/100:.2f} "
+            Fore.YELLOW + "==========================================================="
         )
-        balance += price
-    print(Fore.YELLOW + "---------------------------------------------------------")
-    print(
-        Fore.WHITE + f"TOTAL BALANCE: {Fore.RED} \033[1m${balance / 100:.2f}\033[0m\n"
-    )
+        print(Fore.WHITE + "\033[1mItems:\033[0m")
+        for list_item in item_list:
+            price = list_item.item.price * list_item.quantity
+            print(
+                f"{list_item.item.name}:\n\t Price: ${list_item.item.price/100:.2f} * Quantity: {list_item.quantity} = ${price/100:.2f} "
+            )
+            balance += price
+        print(Fore.YELLOW + "---------------------------------------------------------")
+        print(
+            Fore.WHITE
+            + f"TOTAL BALANCE: {Fore.RED} \033[1m${balance / 100:.2f}\033[0m\n"
+        )
+    else:
+        print(
+            f"""<div class='receipt'>
+            <h3>Receipt for <strong>{customer_name}<strong></h3>
+            <hr>"""
+        )
+        if len(item_list) > 0:
+            print(
+                """<table>
+                <thead>
+                    <tr><th>Item Name</th><th>Price</th><th>Quantity</th><th>Total</th></tr>
+                <thead>
+                <tbody>"""
+            )
+            for list_item in item_list:
+                price = list_item.item.price * list_item.quantity
+                print(
+                    f"<tr><td>{list_item.item.name}</td><td>${list_item.item.price/100:.2f}</td><td>{list_item.quantity}</td><td></td>${price/100:.2f}</tr>"
+                )
+                balance += price
+            print(f"</tbody></table>")
+            print(f"<h4>Total: ${balance / 100:.2f}</h4>")
 
 
 def main():
@@ -45,6 +76,16 @@ def main():
             OrderRow(item=inventory.CHEESE, quantity=5),
             OrderRow(item=inventory.MILK, quantity=3),
         ],
+    )
+    print_receipt(
+        customer_name="Peter Griffin",
+        item_list=[
+            OrderRow(item=inventory.BEEF, quantity=2),
+            OrderRow(item=inventory.LUCKY_CHARMS),
+            OrderRow(item=inventory.CHEESE, quantity=5),
+            OrderRow(item=inventory.MILK, quantity=3),
+        ],
+        html=True,
     )
 
 
