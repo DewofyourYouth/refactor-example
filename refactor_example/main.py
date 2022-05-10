a4329a29296555d210fa07464b142f1d29897a01
Jacob Shore <jacobshore@gmail.com>
2022-05-10 17:12:59 +0300

    sample orders func

--- refactor_example/main.py
+++ refactor_example/main.py
@@ -2,16 +2,15 @@ from refactor_example.inventory import BEEF, BREAD, CHEESE, LUCKY_CHARMS, MILK
 from refactor_example.order import Order, OrderRow
 
 
-def main():
-    order0 = Order(
+def sample_orders():
+    return Order(
         customer_name="Joe Swanson",
         order_items=[
             OrderRow(item=MILK, quantity=2),
             OrderRow(item=BREAD, quantity=1),
             OrderRow(item=CHEESE),
         ],
-    )
-    order1 = Order(
+    ), Order(
         customer_name="Peter Griffin",
         order_items=[
             OrderRow(item=BEEF, quantity=2),
@@ -20,6 +19,10 @@ def main():
             OrderRow(item=MILK, quantity=3),
         ],
     )
+
+
+def main():
+    order0, order1 = sample_orders()
     print(order0.generate_terminal_receipt())
     print(order1.generate_terminal_receipt())
     print(order0.generate_html_receipt())
