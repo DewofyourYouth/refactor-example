8443118a03b1f967d066bbd765d8f1380827f6b7
jshore_ironscales <jshore@ironscales.com>
2022-05-12 16:58:38 +0300

    accurate version

--- tests/test_refactor_example.py
+++ tests/test_refactor_example.py
@@ -2,6 +2,6 @@ from pytest import mark
 from refactor_example import __version__
 
 
-@mark.it("Check that the version is correct.")
+@mark.it("The version is accurate.")
 def test_version():
     assert __version__ == "0.1.1"
