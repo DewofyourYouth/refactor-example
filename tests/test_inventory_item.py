f21458b8c071ab6b93381a748b72845ae37677df
jshore_ironscales <jshore@ironscales.com>
2022-05-12 18:28:35 +0300

    test inventory

--- tests/test_inventory_item.py
+++ tests/test_inventory_item.py
@@ -15,7 +15,6 @@ class TestInventoryItem:
     @mark.it("InventoryItem.from_json takes a dict and returns an inventory item")
     def test_load_inventory_item(self):
         assert InventoryItem.from_json(milk) == InventoryItem(
-            item_id="fe865a93-cdee-4d6d-9716-97d2afdf7b7c",
             name="Whole Milk",
             price=416,
             category="Dairy",
@@ -24,40 +23,35 @@ class TestInventoryItem:
 
 
 @mark.it(
-    "test_load_inventory_items_from_file can load InventoryItems from a json file."
+    "test_load_inventory_items_from_file can load InventoryItems from 'inventory_items.json'."
 )
 def test_load_inventory_items_from_file():
-    assert load_inventory_items_from_file("inventory_items.json") == [
+    assert load_inventory_items() == [
         InventoryItem(
-            item_id="fe865a93-cdee-4d6d-9716-97d2afdf7b7c",
             name="Whole Milk",
             price=416,
             category="Dairy",
             volume=Measurement(unit="Gallon", amount=1),
         ),
         InventoryItem(
-            item_id="8efdbf5e-40cb-42f8-899a-22f7cfa32679",
             name="American Processed Cheese",
             price=389,
             category="Dairy",
             volume=Measurement(unit="Pound", amount=1),
         ),
         InventoryItem(
-            item_id="3a291734-3320-406e-84f9-b230f135f636",
             name="White Bread",
             price=250,
             category="Bread",
             volume=Measurement(unit="Loaf", amount=1),
         ),
         InventoryItem(
-            item_id="b286e9d7-6e24-452d-9608-1de4422d6ee3",
             name="Lucky Charms",
             price=1222,
             category="Breakfast Cereal",
             volume=Measurement(unit="Gallon", amount=1),
         ),
         InventoryItem(
-            item_id="660e0b0e-546f-4392-8943-2d7e73942100",
             name="Beef Choice Angus Ribeye Steak",
             price=2270,
             category="Meat & Seafood",
