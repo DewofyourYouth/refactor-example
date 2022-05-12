1e4b0a0a8039926ade7e64a066b8000fdf83244c
jshore_ironscales <jshore@ironscales.com>
2022-05-12 16:41:05 +0300

    increment version

--- tests/test_refactor_example.py
+++ tests/test_refactor_example.py
@@ -1,5 +1,7 @@
+from pytest import mark
 from refactor_example import __version__
 
 
+@mark.it("Check that the version is correct.")
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
